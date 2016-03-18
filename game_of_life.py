f639342a6d7d8daf93e92529effa87ef6d388679
Obiora Amobi <obiora.amobi@gmail.com>
2016-03-18 15:02:26 +0000

    Surviving Cells/ Tests

--- game_of_life.py
+++ game_of_life.py
@@ -1,8 +1,29 @@
-import unittest
+def next(cells):
+    return survivors(cells) + come_to_life(cells)
 
-from game_of_life import test
+def neighbours_of(x, y):
+    return [
+        (x-1,y-1), (x,y-1), (x+1,y-1),
+        (x-1,y),            (x+1,y),
+        (x-1,y+1), (x,y+1), (x+1,y+1)
+    ]
 
-class test_game_of_life(unittest.TestCase):
 
-    def test_is_this_thing_on(self):
-        self.assertEqual()
+def living_neighbours(cells, (x, y)):
+    potential_neighbours = neighbours_of(x, y)
+    return set(potential_neighbours).intersection(cells)
+
+
+def number_of_living_neighbours(cells, cell):
+    return len(living_neighbours(cells, cell))
+
+
+def cell_survives(cells, cell):
+    neighbours = number_of_living_neighbours(cells, cell)
+    return neighbours == 2 or neighbours == 3
+
+def survivors(cells):
+    return [cell for cell in cells if cell_survives(cells, cell)]
+
+def come_to_life(cells):
+    return []
--- test_game_of_life.py
+++ test_game_of_life.py
@@ -1,1 +1,36 @@
-def test(x): return x
+import unittest
+
+from game_of_life import *
+
+class test_game_of_life(unittest.TestCase):
+
+    def test_next_generation_of_empty_pattern_is_empty(self):
+        self.assertEquals(next([]), [])
+
+    def test_a_single_cell_dies(self):
+        self.assertEquals(next([(0,0)]), [])
+
+    def test_a_cell_with_four_neighbours_dies(self):
+        pattern = [
+                (-1, -1),       (1, -1),
+                         (0, 0),
+                (-1, 1),        (1, 1)
+                ]
+        self.assertEquals(next(pattern), [])
+
+    def test_a_single_cell_has_8_neighbouring_positions(self):
+        count = len(neighbours_of(0,0))
+        self.assertEquals(count, 8)
+
+    def test_can_count_live_neighbours_of_a_cell(self):
+        pattern = [
+                (-1, -1),       (1, -1),
+                         (0, 0),
+                (-1, 1),        (1, 1)
+                ]
+        self.assertEquals(number_of_living_neighbours(pattern, (0, 0)), 4)
+
+
+    def test_a_cell_with_two_neighbours_survives(self):
+        pattern = [(-1, 0), (0,0), (1, 0)]
+        self.assertEquals(next(pattern), [(0, 0)])
